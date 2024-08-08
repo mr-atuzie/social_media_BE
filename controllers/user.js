c657b9884f7c4d237c96acdf6411745c196ece38
mr-atuzie <user email>
2024-08-08 07:59:53 +0100

    register user controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,56 @@
 const asyncHandler = require("express-async-handler");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+};
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.send("REGISTER USER");
+  const { name, username, email, password } = req.body;
+
+  if (!name || !email || !username || !password) {
+    res.status(400);
+    throw new Error("please fill in all fields");
+  }
+
+  if (password.length < 6) {
+    res.status(400);
+    throw new Error("Password must be up to 6 characters");
+  }
+
+  const checkUsername = await User.find({ username });
+
+  if (checkUsername) {
+    res.status(400);
+    throw new Error("username name has be taken");
+  }
+
+  const checkEmail = await User.find({ email });
+
+  if (checkEmail) {
+    res.status(400);
+    throw new Error("email has alredy been used");
+  }
+
+  const user = await User.create({ name, username, email, password });
+
+  if (user) {
+    const token = generateToken(user._id);
+
+    res.cookie("token", token, {
+      path: "/",
+      httpOnly: true,
+      expires: new Date(Date.now() + 1000 * 86400),
+      sameSite: "none",
+      secure: true,
+    });
+
+    res.status(201).json(user);
+  } else {
+    res.status(400);
+    throw new Error("unable to register user");
+  }
 });
 
 const loginUser = asyncHandler(async (req, res) => {
